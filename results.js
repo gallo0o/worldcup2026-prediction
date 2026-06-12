a6ae4bd6dcd5d1c31929088461f9e5a53b86f1f8
José Jiménez <99907380+jjimenezgarcia@users.noreply.github.com>
2026-06-12 10:36:34 +0200

    resultados

--- results.js
+++ results.js
@@ -16,8 +16,7 @@ const RESULTS = {
   "thirdPlace": [],
   "groupMatches": {
     "A": {
-      "Mexico__South Africa": { "home": 2, "away": 0 },
-      "South Korea__Czech Republic": { "home": 2, "away": 1 }
+      "Mexico__South Africa": { "home": 2, "away": 0 }
     },
     "B": {},
     "C": {},
@@ -43,104 +42,7 @@ const RESULTS = {
     "final": "",
     "thirdPlace": "",
     "matches": {
-      "round32": [
-        {
-          "match": 73,
-          "team1": "South Korea",
-          "team2": "Bosnia & Herzegovina",
-          "winner": null
-        },
-        {
-          "match": 74,
-          "team1": "Germany",
-          "team2": null,
-          "winner": null
-        },
-        {
-          "match": 75,
-          "team1": "Netherlands",
-          "team2": "Morocco",
-          "winner": null
-        },
-        {
-          "match": 76,
-          "team1": "Brazil",
-          "team2": "Japan",
-          "winner": null
-        },
-        {
-          "match": 77,
-          "team1": "France",
-          "team2": null,
-          "winner": null
-        },
-        {
-          "match": 78,
-          "team1": "Curaçao",
-          "team2": "Senegal",
-          "winner": null
-        },
-        {
-          "match": 79,
-          "team1": "Mexico",
-          "team2": null,
-          "winner": null
-        },
-        {
-          "match": 80,
-          "team1": "England",
-          "team2": null,
-          "winner": null
-        },
-        {
-          "match": 81,
-          "team1": "USA",
-          "team2": null,
-          "winner": null
-        },
-        {
-          "match": 82,
-          "team1": "Belgium",
-          "team2": null,
-          "winner": null
-        },
-        {
-          "match": 83,
-          "team1": "DR Congo",
-          "team2": "Croatia",
-          "winner": null
-        },
-        {
-          "match": 84,
-          "team1": "Spain",
-          "team2": "Algeria",
-          "winner": null
-        },
-        {
-          "match": 85,
-          "team1": "Canada",
-          "team2": null,
-          "winner": null
-        },
-        {
-          "match": 86,
-          "team1": "Argentina",
-          "team2": "Cape Verde",
-          "winner": null
-        },
-        {
-          "match": 87,
-          "team1": "Portugal",
-          "team2": null,
-          "winner": null
-        },
-        {
-          "match": 88,
-          "team1": "Paraguay",
-          "team2": "Egypt",
-          "winner": null
-        }
-      ],
+      "round32": [],
       "round16": [],
       "quarterfinals": [],
       "semifinals": [],
