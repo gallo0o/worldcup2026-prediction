cb9be01b54cfe47612333de8591bc4f532c993cd
Iago Barroso Ortega <148441502+gallo0o@users.noreply.github.com>
2026-06-15 09:44:07 +0200

    Update match results 14/06

--- results.js
+++ results.js
@@ -121,17 +121,17 @@ const RESULTS = {
       "Curaçao__Germany": { "home": 7, "away": 1 },
       //"Curaçao__Ivory Coast": { "home": 1, "away": 3 },
       //"Ecuador__Germany": { "home": 2, "away": 1 },
-      //"Ecuador__Ivory Coast": { "home": 0, "away": 2 },
+      "Ecuador__Ivory Coast": { "home": 1, "away": 0 },
       //"Germany__Ivory Coast": { "home": 0, "away": 1 }
-    },/*
-    "F": {
-      "Japan__Netherlands": { "home": 3, "away": 1 },
-      "Japan__Tunisia": { "home": 1, "away": 1 },
-      "Japan__Sweden": { "home": 2, "away": 2 },
-      "Netherlands__Tunisia": { "home": 1, "away": 0 },
-      "Netherlands__Sweden": { "home": 1, "away": 0 },
-      "Tunisia__Sweden": { "home": 1, "away": 3 }
     },
+    "F": {
+      "Japan__Netherlands": { "home": 2, "away": 2 },
+      //"Japan__Tunisia": { "home": 1, "away": 1 },
+      //"Japan__Sweden": { "home": 2, "away": 2 },
+      //"Netherlands__Tunisia": { "home": 1, "away": 0 },
+      //"Netherlands__Sweden": { "home": 1, "away": 0 },
+      "Tunisia__Sweden": { "home": 5, "away": 1 }
+    },/*
     "G": {
       "Egypt__Belgium": { "home": 2, "away": 1 },
       "Egypt__Iran": { "home": 2, "away": 1 },
