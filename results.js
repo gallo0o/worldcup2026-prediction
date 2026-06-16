14ce813f14b3428df28f960a51a8dd0b559a1af7
Iago Barroso Ortega <148441502+gallo0o@users.noreply.github.com>
2026-06-16 09:04:38 +0200

    Update match results 15/06

--- results.js
+++ results.js
@@ -133,16 +133,16 @@ const RESULTS = {
       "Tunisia__Sweden": { "home": 5, "away": 1 }
     },
     "G": {
-      //"Egypt__Belgium": { "home": 2, "away": 1 },
+      "Egypt__Belgium": { "home": 1, "away": 1 },
       //"Egypt__Iran": { "home": 2, "away": 1 },
       //"Egypt__New Zealand": { "home": 3, "away": 1 },
       //"Belgium__Iran": { "home": 1, "away": 0 },
       //"Belgium__New Zealand": { "home": 1, "away": 0 },
-      //"Iran__New Zealand": { "home": 1, "away": 2 }
+      "Iran__New Zealand": { "home": 2, "away": 2 }
     },
     "H": {
       //"Uruguay__Spain": { "home": 1, "away": 0 },
-      //"Uruguay__Saudi Arabia": { "home": 3, "away": 1 },
+      "Uruguay__Saudi Arabia": { "home": 1, "away": 1 },
       //"Uruguay__Cape Verde": { "home": 0, "away": 0 },
       //"Spain__Saudi Arabia": { "home": 0, "away": 1 },
       "Spain__Cape Verde": { "home": 0, "away": 0 },
