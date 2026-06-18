cb71f5b7e7425a5a6b9e0791684f188a9589f363
José Jiménez <99907380+jjimenezgarcia@users.noreply.github.com>
2026-06-18 21:03:53 +0200

    resultados

--- results.js
+++ results.js
@@ -17,7 +17,8 @@ const RESULTS = {
   "groupMatches": {
     "A": {
       "Mexico__South Africa": { "home": 2, "away": 0 },
-      "South Korea__Czech Republic": { "home": 2, "away": 1 }
+      "South Korea__Czech Republic": { "home": 2, "away": 1 },
+      "Czech Republic__South Africa": { "home": 1, "away": 1 }
     },
     "B": {
       "Canada__Bosnia & Herzegovina": { "home": 1, "away": 1 },
