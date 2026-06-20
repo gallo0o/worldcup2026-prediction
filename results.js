d2d774a6101bb6c5734875bd3db7106af39e390a
Iago Barroso Ortega <148441502+gallo0o@users.noreply.github.com>
2026-06-21 00:56:07 +0200

    Update match results for Germany and Netherlands

--- results.js
+++ results.js
@@ -122,14 +122,14 @@ const RESULTS = {
       //"Curaçao__Ivory Coast": { "home": 1, "away": 3 },
       //"Ecuador__Germany": { "home": 2, "away": 1 },
       "Ecuador__Ivory Coast": { "home": 1, "away": 0 },
-      //"Germany__Ivory Coast": { "home": 0, "away": 1 }
+      "Germany__Ivory Coast": { "home": 2, "away": 1 }
     },
     "F": {
       "Japan__Netherlands": { "home": 2, "away": 2 },
       //"Japan__Tunisia": { "home": 1, "away": 1 },
       //"Japan__Sweden": { "home": 2, "away": 2 },
       //"Netherlands__Tunisia": { "home": 1, "away": 0 },
-      //"Netherlands__Sweden": { "home": 1, "away": 0 },
+      "Netherlands__Sweden": { "home": 5, "away": 1 },
       "Tunisia__Sweden": { "home": 5, "away": 1 }
     },
     "G": {
