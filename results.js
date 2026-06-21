5518c9b526abf839e5afe5389a7054028a4c8512
José Jiménez <99907380+jjimenezgarcia@users.noreply.github.com>
2026-06-21 13:54:30 +0200

    resultados

--- results.js
+++ results.js
@@ -41,11 +41,15 @@ const RESULTS = {
     },
     "E": {
       "Germany__Curaçao": { "home": 7, "away": 1 },
-      "Ivory Coast__Ecuador": { "home": 1, "away": 0 }
+      "Ivory Coast__Ecuador": { "home": 1, "away": 0 },
+      "Germany__Ivory Coast": { "home": 2, "away": 1 },
+      "Ecuador__Curaçao": { "home": 0, "away": 0 }
     },
     "F": {
       "Netherlands__Japan": { "home": 2, "away": 2 },
-      "Sweden__Tunisia": { "home": 5, "away": 1 }
+      "Sweden__Tunisia": { "home": 5, "away": 1 },
+      "Netherlands__Sweden": { "home": 5, "away": 1 },
+      "Tunisia__Japan": { "home": 0, "away": 4 }
     },
     "G": {
       "Belgium__Egypt": { "home": 1, "away": 1 },
