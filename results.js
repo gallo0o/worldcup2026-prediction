025e99365eff253cf0c6fee64c0ea0062858a514
José Jiménez <99907380+jjimenezgarcia@users.noreply.github.com>
2026-06-24 21:57:01 +0200

    resultados

--- results.js
+++ results.js
@@ -65,16 +65,21 @@ const RESULTS = {
     },
     "I": {
       "France__Senegal": { "home": 3, "away": 1 },
-      "Iraq__Norway": { "home": 1, "away": 4 }
+      "Iraq__Norway": { "home": 1, "away": 4 },
+      "France__Iraq": { "home": 3, "away": 0 },
+      "Norway__Senegal": { "home": 3, "away": 2 }
     },
     "J": {
       "Argentina__Algeria": { "home": 3, "away": 0 },
       "Austria__Jordan": { "home": 3, "away": 1 },
-      "Argentina__Austria": { "home": 2, "away": 0 }
+      "Argentina__Austria": { "home": 2, "away": 0 },
+      "Jordan__Algeria": { "home": 1, "away": 2 }
     },
     "K": {
       "Portugal__DR Congo": { "home": 1, "away": 1 },
-      "Uzbekistan__Colombia": { "home": 1, "away": 3 }
+      "Uzbekistan__Colombia": { "home": 1, "away": 3 },
+      "Portugal__Uzbekistan": { "home": 5, "away": 0 },
+      "Colombia__DR Congo": { "home": 1, "away": 0 }
     },
     "L": {
       "England__Croatia": { "home": 4, "away": 2 },
